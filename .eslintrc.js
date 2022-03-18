a880985faacb59747ba1199228614bb16ede9f44
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-03-18 23:00:46 +0800

    fix token import eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true
-  },
-  "extends": [
-      "eslint:recommended",
-      "plugin:vue/essential",
-  ],
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
-  },
-  "plugins": [
-      "vue",
-  ],
-  parser: "vue-eslint-parser",
-  "rules": {
-      "no-unused-vars": "off",
-      'no-useless-escape': 'off',
-      "no-undef": "off",
-  },
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "plugins": [
+        "vue",
+    ],
+    parser: "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "babel-eslint",
+    },
+    "rules": {
+        "no-unused-vars": "off",
+        'no-useless-escape': 'off',
+        "no-undef": "off",
+    },
 };
